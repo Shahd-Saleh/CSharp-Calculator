a820718150e6d814a255b81e4d470c06628157f5
Shahd-Saleh <salehshahd2006@gmail.com>
2025-07-26 07:16:48 +0300

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_App
+{
+    public class Calculator
+    {
+        //first operation
+        public float Add(float num1,float num2)
+        {
+            return num1 + num2;
+        }
+
+        //second operation
+        public float Subtract(float num1, float num2)
+        {
+            return num1 - num2;
+        }
+
+        //third operation
+        public float multiply(float num1, float num2)
+        {
+            return num1 * num2;
+        }
+
+        //fourth operation
+        public float divide(float num1, float num2)
+        {
+            if (num2 == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                throw new DivideByZeroException("Cannot divide by zero.");
+            }
+            return num1 / num2;
+        }
+
+        //fifth operation
+        public float Modulus(float num1, float num2)
+        {
+            return num1 % num2;
+        }
+        //sixth operation
+        public float Power(float num1, float num2)
+        {
+            return (float)Math.Pow(num1, num2);
+        }
+      
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_App
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator=new Calculator();
+
+            bool keepCalculate = true;
+
+            Console.BackgroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("***Welcome To The Calculator ! ***");
+            Console.ResetColor();
+
+            //The loop of the program
+
+            while (keepCalculate)
+            {
+                //taking inputs from user
+               
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Enter The First Number ..");
+                float num1 = float.Parse(Console.ReadLine());
+                Console.WriteLine("Enter The Second Number ..");
+                float num2 = float.Parse(Console.ReadLine());
+              
+
+                Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                Console.WriteLine("Chosse Operation (+) , (-) , (*) , (/) ,(%) ,(^) ");
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                string operation = Console.ReadLine().Trim();
+                float result = 0;
+
+
+                //switch case for operations
+
+                switch (operation)
+                {
+                    case "+":
+                        result = calculator.Add(num1, num2);
+                        break;
+                    case "-":
+                        result = calculator.Subtract(num1, num2);
+                        break;
+                    case "*":
+                        result = calculator.multiply(num1, num2);
+                        break;
+                    case "/":
+                        try
+                        {
+                            result = calculator.divide(num1, num2);
+                        }
+                        catch (DivideByZeroException ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                            continue;
+                        }
+                        break;
+                    case "%":
+                        result = calculator.Modulus(num1, num2);
+                        break;
+                    case "^":
+                        result = calculator.Power(num1, num2);
+                        break;
+                   
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Invalid operation...");
+                        continue;
+                }
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Result = {result}");
+
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                Console.WriteLine("Calculate Again ? Y  /  N ");
+                string choice = Console.ReadLine().ToUpper().Trim();
+                if (choice != "Y")
+                {
+                    keepCalculate = false; // to end the loop 
+                    Console.ResetColor();
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Thank You For Using The Calculator !");
+                }
+                Console.ResetColor();
+            }
+            Console.ReadKey();
+
+            //Thank You For Your Time ! :) 
+
+
+
+
+        }
+    }
+}
